01d591ed12a14cf817348b64da72bd2bc7271d81
Arun Persaud <apersaud@lbl.gov>
2015-10-08 13:25:20 -0700

    Use termcolor for coloring text

--- warp_test/runalltests.py
+++ warp_test/runalltests.py
@@ -5,16 +5,22 @@ It will print out each file name and whether all of the tests passed or not.
 import glob
 from subprocess import Popen, PIPE
 
+try:
+    from termcolor import colored
+except:
+    def colored(a, b):
+        return a
+
 # --- Run each of the files independently.
 for f in glob.glob('*_test.py'):
     p = Popen(["python", f], stdout=PIPE, stderr=PIPE, close_fds=True)
     serr = p.stderr.readlines()
     if serr[-1] == 'OK\n':
-        print('%s OK\n'%f)
+        print('%s %s'%(f, colored('OK', 'green')))
     else:
-        print('\033[1;31m%s\033[0m'%(f))
+        print(colored(f, 'red'))
         for e in serr[:-1]:
             print(e)
         # --- Use ASCII codes to print this is red
-        print('\033[1;31m%s %s\033[0m'%(f,serr[-1]))
+        print('%s %s'%(colored(f, 'red'), colored(serr[-1], 'red')))
 
