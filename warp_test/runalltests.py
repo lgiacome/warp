47762182ea2a8d49d9d830d681514bc361784599
Arun Persaud <apersaud@lbl.gov>
2015-10-08 13:20:51 -0700

    use glob.glob instead of os.listdir and re.match

--- warp_test/runalltests.py
+++ warp_test/runalltests.py
@@ -3,19 +3,10 @@ This will run all files that have a name with the format *_test.py.
 It will print out each file name and whether all of the tests passed or not.
 """
 import os
-import re
-
-allfiles = os.listdir('.')
-
-# --- Get only the python script files that match the format *_test.py
-testfiles = []
-for f in allfiles:
-    froot,fext = os.path.splitext(f)
-    if fext == '.py' and re.match('.+_test.py',f):
-        testfiles.append(f)
+import glob
 
 # --- Run each of the files independently.
-for f in testfiles:
+for f in glob.glob('*_test.py'):
     fin,fout,ferr = os.popen3('python %s'%f)
     serr = ferr.readlines()
     if serr[-1] == 'OK\n':
