29765d2c47d5524d2a920e6f566cbbfddcda515a
Remi Lehe <remi.lehe@normalesup.org>
2016-10-30 16:07:50 -0700

    Keep np.stack whenever it is available

--- scripts/data_dumping/openpmd_diag/boosted_field_diag.py
+++ scripts/data_dumping/openpmd_diag/boosted_field_diag.py
@@ -538,10 +538,15 @@ class LabSnapshot:
         # Pack the different slices together
         # Reverse the order of the slices when stacking the array,
         # since the slices where registered for right to left
-        index  = np.array(np.shape( self.buffered_slices[::-1] ))
-        rolled_index = np.roll(index, -1)
-        field_array = np.dstack( self.buffered_slices[::-1] )
-        field_array = field_array.reshape( (rolled_index), order="F" )
+        try:
+            field_array = np.stack( self.buffered_slices[::-1], axis=-1 )
+        except AttributeError:
+            # If the version of numpy is older than 1.10, stack does
+            # not exist. In this case, do it by hand:
+            index  = np.array(np.shape( self.buffered_slices[::-1] ))
+            rolled_index = np.roll(index, -1)
+            field_array = np.dstack( self.buffered_slices[::-1] )
+            field_array = field_array.reshape( (rolled_index), order="F" )
 
         # Get the first and last index in z
         # (Following Python conventions, iz_min is inclusive,
