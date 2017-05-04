95adcdc79407e3911bf2b73b9912bde441f8b4ba
Guillaume Blaclard <gblaclard@lbl.gov>
2017-05-04 16:38:14 -0700

    Add the correction brought by G.Blaclard in the PICSAR pull request #85.

--- scripts/field_solvers/em3dsolverPXR.py
+++ scripts/field_solvers/em3dsolverPXR.py
@@ -154,17 +154,14 @@ def addparticlesPXR(self,x=0.,y=0.,z=0.,vx=0.,vy=0.,vz=0.,gi=1.,w=None,
     nps0 = x.size
     pids = np.zeros([nps0,pxr.npid])
 
-    # --- Load in any pid data passed in
-    if pidpairs is None:
-        if top.wpid>0:
-            if w is None:
-               w=np.ones(nps0)
-            pids[:,pxr.wpid-1]=w*self.sw0
-    else:
+    if pidpairs is not None :
         for id,pp in pidpairs:
             pids[:,id-1] = pp
-            if top.wpid>0:
-                pids[:,pxr.wpid-1]*=self.sw0
+
+    if top.wpid >0:
+        if w is None:
+            w=np.ones(nps0)
+        pids[:,pxr.wpid-1]=w*self.sw0
 
     x = array(x)*ones(nps0,'d')
     y = array(y)*ones(nps0,'d')
@@ -250,26 +247,23 @@ def addparticlesPXR(self,x=0.,y=0.,z=0.,vx=0.,vy=0.,vz=0.,gi=1.,w=None,
     if lnewparticles:
         # --- Set time of creation
         if top.tbirthpid > 0:
-            pid[:,top.tbirthpid-1] = top.time
-
-        # --- Set weights
-        if w is not None and top.wpid > 0:
-            pid[:,top.wpid-1] = array(w)*ones(maxlen,'d')
-        # --- Note that ssn is set in addpart
+            pids[:,top.tbirthpid-1] = top.time
 
         # --- Set xyz old
-        if top.xoldpid > 0: pid[:,top.xoldpid-1] = x
-        if top.yoldpid > 0: pid[:,top.yoldpid-1] = y
-        if top.zoldpid > 0: pid[:,top.zoldpid-1] = z
-        if lmomentum:
-            if top.uxoldpid > 0: pid[:,top.uxoldpid-1] = vx
-            if top.uyoldpid > 0: pid[:,top.uyoldpid-1] = vy
-            if top.uzoldpid > 0: pid[:,top.uzoldpid-1] = vz
-        else:
-            if top.uxoldpid > 0: pid[:,top.uxoldpid-1] = vx/gi
-            if top.uyoldpid > 0: pid[:,top.uyoldpid-1] = vy/gi
-            if top.uzoldpid > 0: pid[:,top.uzoldpid-1] = vz/gi
+        if top.xoldpid > 0: pids[:,top.xoldpid-1] = x
+        if top.yoldpid > 0: pids[:,top.yoldpid-1] = y
+        if top.zoldpid > 0: pids[:,top.zoldpid-1] = z
 
+        if lmomentum:
+            if top.uxoldpid > 0: pids[:,top.uxoldpid-1] = vx
+            if top.uyoldpid > 0: pids[:,top.uyoldpid-1] = vy
+            if top.uzoldpid > 0: pids[:,top.uzoldpid-1] = vz
+        else:
+            if top.uxoldpid > 0: pids[:,top.uxoldpid-1] = vx/gi
+            if top.uyoldpid > 0: pids[:,top.uyoldpid-1] = vy/gi
+            if top.uzoldpid > 0: pids[:,top.uzoldpid-1] = vz/gi
+            
+    # --- Call to PICSAR function for adding particles
     pxr.py_add_particles_to_species(self.pxr_species_array, nps0,pxr.npid,
                                     x,
                                     y,
