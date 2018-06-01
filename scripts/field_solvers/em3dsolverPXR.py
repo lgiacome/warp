743ff00e2406cf2f37ccead47ba4a36fbb5e9cbd
haithem_kallala <kallalahaythem@gmail.com>
2018-06-01 14:45:34 +0200

    adapting picsar antenna to warp

--- scripts/field_solvers/em3dsolverPXR.py
+++ scripts/field_solvers/em3dsolverPXR.py
@@ -470,6 +470,266 @@ def getn(self, gather=1, bcast=None, **kw ):
     return nb[0]
 
 
+
+def initialize_virtual_particles( self, w3d ):
+    """
+    Initialization of the antenna particles depending on the dimension and
+    the laser propagation vector.
+    """
+    # Shortcut definition
+    x0 = self.spot[0]
+    y0 = self.spot[1]
+    z0 = self.spot[2]
+    xmin = w3d.xmminlocal
+    xmax = w3d.xmmaxlocal
+    ymin = w3d.ymminlocal
+    ymax = w3d.ymmaxlocal
+    zmin = w3d.zmminlocal
+    zmax = w3d.zmmaxlocal
+
+    if self.dim == "1d":
+        # Ux is chosen orthogonal to self.vector in the plane (x,z)
+        Uy = np.array([0.,1.,0.])
+        Ux = np.cross(Uy,self.vector)
+        self.Ux = Ux
+        self.Uy = Uy
+
+        # 1D injection along x
+        self.nn_global = 1
+        self.xx_global = x0 + np.zeros(self.nn_global)
+        self.yy_global = y0 + np.zeros(self.nn_global)
+        self.zz_global = z0 + np.zeros(self.nn_global)
+
+    elif self.dim == "circ":
+        # 2D circ
+
+        # Check that the normal vector is along z and that the
+        # position of the antenna is on the axis
+        # (Otherwise the simulation cannot be performed in cylindrical
+        # coordinate)
+        assert np.allclose( self.vector, np.array([0,0,1]) )
+        assert np.allclose( self.spot[:1], np.array([0,0]) )
+        # Get the vectors that give the coordinate system of the antenna
+        Ux = self.polvector
+        Uy = self.polvector_2
+        self.Ux = Ux
+        self.Uy = Uy
+
+        # The points of the antenna are along a star-pattern
+        imin = np.floor( xmin/w3d.dx )
+        imax = np.floor( xmax/w3d.dx )
+        rr = w3d.dx * np.arange( imin, imax+1 )
+        self.weights_circ = 2 * np.pi * rr / w3d.dx
+        self.weights_circ /= 4 * self.circ_m
+        w0 = self.weights_circ.copy()
+        self.xx_global = rr.copy()
+        self.yy_global = np.zeros_like( self.xx_global  )
+        for i in range( 1, 4*self.circ_m ):
+            phase = 0.5*np.pi*float(i)/self.circ_m
+            self.xx_global = np.concatenate( (self.xx_global,
+                                                rr*np.cos(phase)) )
+            self.yy_global = np.concatenate( (self.yy_global,
+                                                rr*np.sin(phase)) )
+            self.weights_circ = np.concatenate((self.weights_circ,w0))
+        self.nn_global = np.shape(self.xx_global)[0]
+        self.zz_global = z0 + np.zeros(self.nn_global)
+
+    elif self.dim == "2d":
+        # 2D plane
+
+        # Ux is chosen orthogonal to self.vector in the plane (x,z)
+        Uy = np.array([0.,1.,0.])
+        Ux = np.cross(Uy,self.vector)
+        self.Ux = Ux
+        self.Uy = Uy
+
+        # Spacing between virtual particles to ensure at least one
+        # particle per cell
+        # select only the Ux components different from 0
+        list_Ux = []
+        if not Ux[0] == 0.: list_Ux.append(w3d.dx/np.abs(Ux[0]))
+        if not Ux[1] == 0.: list_Ux.append(w3d.dy/np.abs(Ux[1]))
+        if not Ux[2] == 0.: list_Ux.append(w3d.dz/np.abs(Ux[2]))
+        self.Sx = min(list_Ux)
+
+        # Boundaries of the box, depending on sign of the components of Ux
+        xmin_i = switch_min_max(xmin, xmax, Ux[0])
+        ymin_i = switch_min_max(ymin, ymax, Ux[1])
+        zmin_i = switch_min_max(zmin, zmax, Ux[2])
+        xmax_i = switch_min_max(xmax, xmin, Ux[0])
+        ymax_i = switch_min_max(ymax, ymin, Ux[1])
+        zmax_i = switch_min_max(zmax, zmin, Ux[2])
+
+        # Find the range of integer with which the particles will be
+        # initialized
+        imin = Ux[0]*(xmin_i-x0) + Ux[1]*(ymin_i-y0) + Ux[2]*(zmin_i-z0)
+        imax = Ux[0]*(xmax_i-x0) + Ux[1]*(ymax_i-y0) + Ux[2]*(zmax_i-z0)
+        imin = np.floor(imin/self.Sx)
+        imax = np.floor(imax/self.Sx)+1
+        antenna_i = np.arange(imin, imax)
+
+        # Initialize the particle positions
+        self.xx_global = x0 + self.Sx*Ux[0]*antenna_i
+        self.zz_global = z0 + self.Sx*Ux[2]*antenna_i
+
+        # Keep only the particles that are inside the global box
+        is_in_global_box = (self.xx_global >= xmin) \
+                            & (self.xx_global < xmax) \
+                            & (self.zz_global >= zmin) \
+                            & (self.zz_global < zmax)
+        self.zz_global = self.zz_global[is_in_global_box]
+        self.xx_global = self.xx_global[is_in_global_box]
+        self.yy_global = np.zeros(len(self.xx_global))
+        # Number of virtual particles
+        self.nn_global = np.shape(self.xx_global)[0]
+
+    else:
+        # 3D case, Ux = polvector and Uy = polvector_2
+        Ux = self.polvector
+        Uy = self.polvector_2
+        self.Ux = Ux
+        self.Uy = Uy
+
+        # Spacing between virtual particles to ensure at least
+        # one particle per cell
+        # select only the components of Ux and Uy different from 0
+        list_Ux = []; list_Uy = []
+        if not Ux[0] == 0.: list_Ux.append( w3d.dx/np.abs(Ux[0]) )
+        if not Ux[1] == 0.: list_Ux.append( w3d.dy/np.abs(Ux[1]) )
+        if not Ux[2] == 0.: list_Ux.append( w3d.dz/np.abs(Ux[2]) )
+        if not Uy[0] == 0.: list_Uy.append( w3d.dx/np.abs(Uy[0]) )
+        if not Uy[1] == 0.: list_Uy.append( w3d.dy/np.abs(Uy[1]) )
+        if not Uy[2] == 0.: list_Uy.append( w3d.dz/np.abs(Uy[2]) )
+        self.Sx = min(list_Ux)
+        self.Sy = min(list_Uy)
+
+        # Boundaries of the box, depending on sign of the components of Ux
+        xmin_i = switch_min_max(xmin, xmax, Ux[0])
+        ymin_i = switch_min_max(ymin, ymax, Ux[1])
+        zmin_i = switch_min_max(zmin, zmax, Ux[2])
+        xmax_i = switch_min_max(xmax, xmin, Ux[0])
+        ymax_i = switch_min_max(ymax, ymin, Ux[1])
+        zmax_i = switch_min_max(zmax, zmin, Ux[2])
+        # Boundaries of the box, depending on sign of the components of Uy
+        xmin_j = switch_min_max(xmin, xmax, Uy[0])
+        ymin_j = switch_min_max(ymin, ymax, Uy[1])
+        zmin_j = switch_min_max(zmin, zmax, Uy[2])
+        xmax_j = switch_min_max(xmax, xmin, Uy[0])
+        ymax_j = switch_min_max(ymax, ymin, Uy[1])
+        zmax_j = switch_min_max(zmax, zmin, Uy[2])
+
+        # Find the range of integer with which the particles will be
+        # initialized
+        imin = Ux[0]*(xmin_i-x0) + Ux[1]*(ymin_i-y0) + Ux[2]*(zmin_i-z0)
+        imax = Ux[0]*(xmax_i-x0) + Ux[1]*(ymax_i-y0) + Ux[2]*(zmax_i-z0)
+        jmin = Uy[0]*(xmin_j-x0) + Uy[1]*(ymin_j-y0) + Uy[2]*(zmin_j-z0)
+        jmax = Uy[0]*(xmax_j-x0) + Uy[1]*(ymax_j-y0) + Uy[2]*(zmax_j-z0)
+        imin = np.floor(imin/self.Sx)
+        imax = np.floor(imax/self.Sx)+1
+        jmin = np.floor(jmin/self.Sy)
+        jmax = np.floor(jmax/self.Sy)+1
+        array_i = np.arange(imin, imax)
+        array_j = np.arange(jmin, jmax)
+        antenna_i, antenna_j = np.meshgrid(array_i,array_j)
+
+        # Initialize the particle positions
+        self.xx_global = x0 + self.Sx*Ux[0]*antenna_i + self.Sy*Uy[0]*antenna_j
+        self.yy_global = y0 + self.Sx*Ux[1]*antenna_i + self.Sy*Uy[1]*antenna_j
+        self.zz_global = z0 + self.Sx*Ux[2]*antenna_i + self.Sy*Uy[2]*antenna_j
+        self.xx_global = self.xx_global.flatten()
+        self.yy_global = self.yy_global.flatten()
+        self.zz_global = self.zz_global.flatten()
+
+        # Keep only the particles that are inside the global box
+        is_in_global_box = (self.xx_global >= xmin) \
+                            & (self.xx_global < xmax) \
+                            & (self.yy_global >= ymin) \
+                            & (self.yy_global < ymax) \
+                            & (self.zz_global >= zmin) \
+                            & (self.zz_global < zmax)
+        self.zz_global = self.zz_global[is_in_global_box]
+        self.yy_global = self.yy_global[is_in_global_box]
+        self.xx_global = self.xx_global[is_in_global_box]
+        # Number of virtual particles
+        self.nn_global = np.shape(self.xx_global)[0]
+
+    # Set the deplacement around the initial position and normalized momenta
+    # variation of each macroparticles to 0
+    self.xdx_global = np.zeros(self.nn_global)
+    self.ydy_global = np.zeros(self.nn_global)
+    self.zdz_global = np.zeros(self.nn_global)
+    self.ux_global = np.zeros(self.nn_global)
+    self.uy_global = np.zeros(self.nn_global)
+    self.uz_global = np.zeros(self.nn_global)
+    self.gi_global = np.ones(self.nn_global)
+
+    # Calculate the weights
+    self.weights_global = np.ones(self.nn_global) * epsilon_0*self.emax/0.01
+
+    if self.dim == "2d":
+        self.weights_global *= self.Sx
+    elif self.dim == "3d" :
+        self.weights_global *= self.Sy*self.Sx
+    elif self.circ_m > 0 : # Circ
+        # Laser initialized with particles in a star-pattern
+        self.weights_global *= w3d.dx**2 * self.weights_circ
+
+    js_laser_pos = numpy.empty(1,dtype=numpy.int64)
+    js_laser_neg = numpy.empty(1,dtype=numpy.int64)
+   
+    pxr.init_laser_species_python(self.emax, self.spot, self.vector, polvector1,polvector2, +1., \
+    self.weight_global[0], self.xx_global, self.yy_global, self.zz_global, self.nn_global, js_laser_pos)
+
+    pxr.init_laser_species_python(self.emax, self.spot, self.vector, polvector1,polvector2, -1., \
+    self.weight_global[0], self.xx_global, self.yy_global, self.zz_global, self.nn_global, js_laser_neg)
+
+    self.js_pos = js_laser_pos
+    self.js_neg = js_laser_neg
+
+def push_virtual_particles(self, top, f, clight ):
+    """
+    Calculate the motion parameters of the laser antenna at a given
+    timestep
+    """
+    
+    nb = numpy.empty(1,dtype=numpy.int64)
+    pxr.get_local_number_of_particles_from_species(self.js_pos, nb )
+
+    quantity_array = numpy.empty(nb[0], dtype=numpy.float64, order='F')
+
+    dt = top.dt
+
+    # Coordinate of the antenna in the plane (Ux,Uy)
+    wpid = top.wpid
+    x = pxr.getquantity_pid(self.js, wpid+1, nb, quantity_array)
+    y = pxr.getquantity_pid(self.js, wpid+2, nb, quantity_array)
+    t = top.time*(1.-self.v/clight)
+    amp = self.laser_func(x,y,t)
+
+    # --- displaces fixed weight particles on "continuous" trajectories
+    dispmax = 0.01*clight
+    coef_ampli = dispmax * (1.-self.v/clight) / self.emax
+
+    if isinstance(amp,list): #elliptic polarization
+        amp_x = amp[0]*self.polvector[0] + amp[1]*self.polvector_2[0]
+        amp_y = amp[0]*self.polvector[1] + amp[1]*self.polvector_2[1]
+        amp_z = amp[0]*self.polvector[2] + amp[1]*self.polvector_2[2]
+
+        amplitude_x = coef_ampli * amp_x * 0.5
+        amplitude_y = coef_ampli * amp_y * 0.5
+        amplitude_z = coef_ampli * amp_z * 0.5
+
+    else: #linear polarization
+        amplitude_x = coef_ampli * amp * self.polvector[0] * 0.5
+        amplitude_y = coef_ampli * amp * self.polvector[1] * 0.5
+        amplitude_z = coef_ampli * amp * self.polvector[2] * 0.5
+
+    pxr.laser_pusher_profile(amplitude_x,amplitude_y,amplitude_z,nb)
+
+def select_particles_in_local_box(self, w3d, zgrid):
+    self.nn = 0
+    return
+
 class EM3DPXR(EM3DFFT):
 
     __em3dpxrinputs__ = []
@@ -558,6 +818,11 @@ class EM3DPXR(EM3DFFT):
           Species.getby            = getby
           Species.getbz            = getbz
           Species.getn             = getn
+
+          LaserAntenna.initialize_virtual_particles  = initialize_virtual_particles 
+          LaserAntenna.push_virtual_particles        = push_virtual_particles   
+          LaserAntenna.select_particles_in_local_box = select_particles_in_local_box 
+
 
         else:
           EM3DFFT.finalize(self)
