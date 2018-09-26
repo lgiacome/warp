ebdae384387e9b321c715359f891849294060c06
haithem_kallala <kallalahaythem@gmail.com>
2018-09-26 11:52:48 +0200

    getting rid of useless pml dump in with full pxrr

--- scripts/field_solvers/em3dsolver.py
+++ scripts/field_solvers/em3dsolver.py
@@ -1953,8 +1953,13 @@ class EM3D(SubcycledPoissonSolver):
                 self.push_e_full(i)
                 self.exchange_e()
             self.push_b_part_1()
-        if self.pml_method==2:
-            scale_em3d_bnd_fields(self.block,top.dt,self.l_pushf,self.l_pushg)
+	if(hasattr(self,"full_pxr") == False):
+          if self.pml_method==2:
+              scale_em3d_bnd_fields(self.block,top.dt,self.l_pushf,self.l_pushg)
+	elif(self.full_pxr == False):
+          if self.pml_method==2:
+              scale_em3d_bnd_fields(self.block,top.dt,self.l_pushf,self.l_pushg)
+
         if self.fields.spectral:
             self.exchange_e()
         if self.l_pushf:self.exchange_f()
